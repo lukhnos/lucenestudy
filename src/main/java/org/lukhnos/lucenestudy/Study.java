f8bb46291e78679f882b4e813031ee8b5e58dda4
Lukhnos Liu <lukhnos@lukhnos.org>
2015-08-04 23:42:35 -0700

    Check in project skeleton

---
+++ src/main/java/org/lukhnos/lucenestudy/Study.java
@@ -0,0 +1,7 @@
+package org.lukhnos.lucenestudy;
+
+public class Study {
+  public static void main(String args[]) {
+    System.out.println("hello, world");
+  }
+}
---
+++ src/test/java/org/lukhnos/lucenestudy/TrivialTest.java
@@ -0,0 +1,11 @@
+package org.lukhnos.lucenestudy;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TrivialTest {
+  @Test
+  public void trival() {
+    assertEquals(1, 1);
+  }
+}
