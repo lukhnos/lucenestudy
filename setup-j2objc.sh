dc95bc0e7194eab0ab80f6a5a485a98fa4c90ac5
kevindelord <delord.kevin@gmail.com>
2016-05-18 12:26:04 +0200

    Update setup-j2objc script

--- setup-j2objc.sh
+++ setup-j2objc.sh
@@ -1,27 +1,30 @@
 #!/bin/bash -e
 
 # TODO: Parameterize, error checking, temp file, etc.
-VERSION=0.9.8.1-20150920-24f9bc5
+VERSION=1.0.2
 FILE=j2objc-$VERSION.zip
-URL=https://github.com/lukhnos/j2objc/releases/download/$VERSION/$FILE
+URL=https://github.com/google/j2objc/releases/download/$VERSION/$FILE
+#URL=https://github.com/lukhnos/j2objc/releases/download/$VERSION/$FILE
 DIR=j2objc-$VERSION
-TARGET=vendor
+TARGET=vendor/j2objc
 
 echo Fetching ${URL}
 curl -L -o "${FILE}" "${URL}"
 
-echo Creating dir ${TARGET}
-mkdir -p "${TARGET}"
-
 echo Unzipping ${FILE}
 unzip "${FILE}"
 
-echo Moving ${DIR} to ${TARGET}/j2objc
-mv "${DIR}" "${TARGET}/j2objc"
+echo Remove old ${TARGET} directory
+rm -rf "${TARGET}"
+
+echo Rename ${DIR} into ${TARGET}
+mv "${DIR}" "${TARGET}"
+
+echo Moving ${FILE} archive to ${TARGET}
+mv "${FILE}" "${TARGET}"
 
 echo Making j2objc available to the objclucene submodule
-OBJCLUCENE_VENDOR=vendor/objclucene/vendor
+OBJCLUCENE_VENDOR="${PWD}/vendor/objclucene/vendor"
 mkdir -p "${OBJCLUCENE_VENDOR}"
 rm -f "${OBJCLUCENE_VENDOR}/j2objc"
-ln -s "${PWD}/${TARGET}/j2objc" "${OBJCLUCENE_VENDOR}/j2objc"
-
+ln -s "${PWD}/${TARGET}" "${OBJCLUCENE_VENDOR}/j2objc"
