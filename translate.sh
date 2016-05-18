6538f69ab9892bb9fe57436a7ffd1def95944b2e
kevindelord <delord.kevin@gmail.com>
2016-05-18 12:27:55 +0200

    Update translate script to use local version of j2objc

--- translate.sh
+++ translate.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-j2objc -d src/main/objc -use-arc -cp build/libs/lucenestudy.jar `find src/main/java -name *.java`
-j2objc -d src/test/objc -use-arc -cp src/test/java:build/libs/lucenestudy.jar:$J2OBJC_ROOT/lib/j2objc_junit.jar:$J2OBJC_ROOT/lib/hamcrest-core-1.3.jar `find src/test/java -name *.java`
+#!/bin/bash -e
+
+J2OBJC=./vendor/j2objc/j2objc
+
+${J2OBJC} -d src/main/objc -use-arc -cp build/libs/lucenestudy.jar `find src/main/java -name *.java`
+${J2OBJC} -d src/test/objc -use-arc -cp src/test/java:build/libs/lucenestudy.jar:${J2OBJC}/lib/j2objc_junit.jar:${J2OBJC}/lib/hamcrest-core-1.3.jar `find src/test/java -name *.java`
